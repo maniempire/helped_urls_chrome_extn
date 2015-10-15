241f97f326010e352807c89af282884595594fbc
Manimaran Malaichamy <mani@maniempire.com>
2015-10-15 13:55:08 +0530

    add icon and popup file

--- url_picker.js
+++ url_picker.js
@@ -17,3 +17,23 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender) {
 		console.log("Extension has received a message from from remote server: '" + request.message + "'");
   });
+
+  /* Listen for message from the popup */
+  chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+      /* First, validate the message's structure */
+      if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+		  alert("In if condn");
+          /* Collect the necessary data 
+           * (For your specific requirements `document.querySelectorAll(...)`
+           *  should be equivalent to jquery's `$(...)`) */
+          var domInfo = {
+              // total:   document.querySelectorAll('*').length,
+//               inputs:  document.querySelectorAll('input').length,
+//               buttons: document.querySelectorAll('button').length
+			  test: 4
+          };
+          /* Directly respond to the sender (popup), 
+           * through the specified callback */
+          response(domInfo);
+      }
+  });
